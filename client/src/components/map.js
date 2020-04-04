e115c5deb019d5dbac15626059886fdee41e367d
Borja Domínguez <borja.d.atienza@gmail.com>
2020-04-04 13:11:03 +0200

    map box remove

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -1,22 +1,14 @@
 import React, { useState } from "react";
-import ReactMapGl from "react-map-gl";
+import { TOKEN_API } from "/token_key";
 
-const MapBox_token =
-  "pk.eyJ1Ijoic3Jib3IiLCJhIjoiY2s4a2pwc251MDFtejNubnp6NTgxeXRybCJ9.P4Yku1rTTf4WNS_drkpLrA";
 export const Map = () => {
   const [viewport, setViewPort] = useState({
-    width: 400,
-    height: 400,
+    width: "100vw",
+    height: "100vh",
     latitude: 37.7577,
     longitude: -122.4376,
     zoom: 8
   });
 
-  return (
-    <ReactMapGl
-      {...viewport}
-      onViewportChange={setViewPort}
-      mapboxApiAccessToken={MapBox_token}
-    />
-  );
+  return <div>Aquí va el mapa</div>;
 };
