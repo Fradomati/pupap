80519d17b2d3c6d80d11a30597d234020c96a98b
Borja Domínguez <borja.d.atienza@gmail.com>
2020-04-05 20:38:43 +0200

    Now test react-google-maps

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -1,14 +1,2 @@
 import React, { useState } from "react";
 import { TOKEN_API } from "/token_key";
-
-export const Map = () => {
-  const [viewport, setViewPort] = useState({
-    width: "100vw",
-    height: "100vh",
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8
-  });
-
-  return <div>Aquí va el mapa</div>;
-};
