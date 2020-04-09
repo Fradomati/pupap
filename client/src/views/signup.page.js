31f32069f983e236c490716873236b08567ffe15
Borja Domínguez <borja.d.atienza@gmail.com>
2020-04-09 19:46:25 +0200

    We have Signup

--- client/src/views/signup.page.js
+++ client/src/views/signup.page.js
@@ -1,5 +1,53 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { fnSignup } from "../connects/authConnect";
 
 export const Signup = () => {
-  return <h1>Estás en el Signup</h1>;
+  const { register, handleSubmit, errors } = useForm({
+    mode: "onSubmit"
+  });
+  const onSubmit = async data => {
+    console.log("Datos del usuario:", data);
+    const responseServer = await fnSignup(data);
+    console.log("La promesa", responseServer);
+  };
+
+  console.log(errors);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="text"
+        placeholder="Username"
+        name="username"
+        ref={register({ required: true, maxLength: 80 })}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        name="password"
+        ref={register({
+          required: true,
+          min: 8,
+          maxLength: 12,
+          pattern: /^(?=.*\d).{8,12}$/i
+        })}
+      />
+      {errors.password && (
+        <p>
+          Password must be between 8 and 12 digits long and include at least one
+          numeric digit.
+        </p>
+      )}
+      <input
+        type="text"
+        placeholder="Email"
+        name="email"
+        ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+      />
+      {errors.email && <p>Ups... Ese email no es correcto :(</p>}
+
+      <input type="submit" />
+    </form>
+  );
 };
