43a562c4ac0df0c295628e3d8ab995005c93b317
Borja Domínguez <borja.d.atienza@gmail.com>
2020-04-18 10:36:30 +0200

    a bit of design

--- client/src/views/content.page.js
+++ client/src/views/content.page.js
@@ -1,6 +1,12 @@
 import React, { useState, useContext, useEffect } from "react";
 import { popularCnt, intCnt } from "../connects/cntConnect";
 import { ContextApp } from "../context/Context";
+// Imágenes:
+import int from "../../public/images/int.svg";
+import fut from "../../public/images/fut.svg";
+import ma from "../../public/images/ma.svg";
+import pos from "../../public/images/pos.svg";
+import neg from "../../public/images/neg.svg";
 
 export const Content = () => {
   //const { content } = useContext(ContextApp);
@@ -49,37 +55,50 @@ export const Content = () => {
   } else {
     return (
       <div className="container-enjoy">
-        <button type="button" onClick={() => intBtn()}>
-          +Interesante
-        </button>
+        <div className="head-enjoy">
+          <img src={int} className="icon-img" onClick={() => intBtn()}></img>
+          <img src={fut} className="icon-img" onClick={() => intBtn()}></img>
+          <img src={ma} className="icon-img" onClick={() => intBtn()}></img>
+          <img src={int} className="icon-img" onClick={() => intBtn()}></img>
+        </div>
         <div className="cards-container">
           <ul>
             {newCnt.map((post, i) => (
               <li key={i}>
                 <div className="card">
-                  <div className="section-1">
-                    <div className="card-img">
-                      <img src={post.image} />
-                    </div>
-                    <div className="card-text">
-                      <div className="text">
-                        <p>
-                          <a href={post.url}>{post.title}</a>
-                        </p>
+                  <div>
+                    <div className="section-1">
+                      <div className="card-img">
+                        <img src={post.image} />
                       </div>
-                    </div>
-                  </div>
-                  <div className="section-2">
-                    <div className="card-btn">
-                      <div className="more-info">
-                        <a href="#">+info</a>
-                      </div>
-                      <div className="likes-box">
-                        <div className="likes">
-                          <button>+</button>
+                      <div className="card-text">
+                        <div className="text">
+                          <p>
+                            <a href={post.url}>{post.title}</a>
+                          </p>
                         </div>
-                        <div className="likes">
-                          <button>-</button>
+                      </div>
+                    </div>
+                    <div className="section-2">
+                      <div className="card-btn">
+                        <div className="more-info">
+                          <a href="#">+info</a>
+                        </div>
+                        <div className="likes-box">
+                          <div className="likes">
+                            <img
+                              src={pos}
+                              className="icon-vot"
+                              onClick={() => intBtn()}
+                            ></img>
+                          </div>
+                          <div className="likes">
+                            <img
+                              src={neg}
+                              className="icon-vot"
+                              onClick={() => intBtn()}
+                            ></img>
+                          </div>
                         </div>
                       </div>
                     </div>
